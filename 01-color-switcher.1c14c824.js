e16a0c6531091c160ccfdb431982ac5c149abdc3
myhappyday <111706884+myhappyday@users.noreply.github.com>
2023-03-23 04:27:06 +0000

    Deploying to gh-pages from @ myhappyday/goit-js-hw-09@8b892209868cf73e6f845b3ae4dc9cb22fd73df8 🚀

---
+++ 01-color-switcher.1c14c824.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;function d(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.disabled=!0,t.addEventListener("click",(function(){a||(t.disabled=!0,e.disabled=!1,a=setInterval(d,1e3))})),e.addEventListener("click",(function(){a&&(t.disabled=!1,e.disabled=!0,clearInterval(a),a=null)}))}();
+//# sourceMappingURL=01-color-switcher.1c14c824.js.map
